bef759cfb0a09efea6a578cc1ebcf6144434af7d
edsuuu <111332935+edsuuu@users.noreply.github.com>
2023-10-28 17:49:37 -0300

    bug fix

--- src/Components/Footer/footer-link.jsx
+++ src/Components/Footer/footer-link.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-
 const FooterLink = ({ links }) => (
   <div className="footer-link">
     <ul>
       {links.map((link, index) => (
         <li key={index}>
-          <a href={link.url} target="_blank" rel="noopener noreferrer">{link.name}</a>
+          <a href={link.url} target="_blank" rel="noopener noreferrer">
+            {link.name}
+          </a>
         </li>
       ))}
     </ul>
